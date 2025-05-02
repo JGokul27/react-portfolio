89de98a03535eedd9cfa128cc4ce1aee884d552b
GOKULVCE <gokuljcse2021@gmail.com>
2025-05-02 12:30:26 +0530

    technologies added

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { RiReactjsLine } from "react-icons/ri";
-import { SiMongodb } from "react-icons/si";
-import { FaNodeJs } from "react-icons/fa";
+import { RiJavaFill, RiJavaLine, RiReactjsLine } from "react-icons/ri";
+import { SiMongodb, SiPostman } from "react-icons/si";
+import { FaFigma, FaNodeJs } from "react-icons/fa";
 import { motion } from "framer-motion";
-import { GrMysql } from "react-icons/gr"; 
+import { GrMysql } from "react-icons/gr";
 import { SiExpress } from "react-icons/si";
 import { SiTailwindcss } from "react-icons/si";
-
-
+import { TbBrandFramerMotion } from "react-icons/tb";
+import { DiJavascript } from "react-icons/di";
 
 const iconVariants = (duration) => ({
   initial: { y: -10 },
@@ -25,13 +25,28 @@ const iconVariants = (duration) => ({
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <motion.h1 whileInView={{opacity:1,y:0}} initial={{opacity: 0, y:-100}} transition={{duration:1.5}} className="my-20 text-center text-4xl">Technologies</motion.h1>
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h1>
       <motion.div
         whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: -100 }}
         transition={{ duration: 1.5 }}
         className="flex flex-wrap items-center justify-center gap-4"
       >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiJavascript className="text-7xl text-yellow-300" />
+        </motion.div>
         <motion.div
           variants={iconVariants(2.5)}
           initial="initial"
@@ -65,7 +80,7 @@ const Technologies = () => {
           <SiExpress className="text-7xl text-red-700" />
         </motion.div>
         <motion.div
-          variants={iconVariants(5)}
+          variants={iconVariants(2.5)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
@@ -73,12 +88,44 @@ const Technologies = () => {
           <SiMongodb className="text-7xl text-green-500" />
         </motion.div>
         <motion.div
-          variants={iconVariants(4)}
+          variants={iconVariants(3)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <GrMysql className="text-7xl text-sky-700" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiJavaFill className="text-7xl text-orange-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(1.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandFramerMotion className="text-7xl text-purple-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(1.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaFigma className="text-7xl text-red-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(1.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiPostman className="text-7xl text-orange-400" />
         </motion.div>
       </motion.div>
     </div>
